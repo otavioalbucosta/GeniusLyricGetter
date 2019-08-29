988c33858d6cf91832587d3a41c81738ed5d0508
otavioalbucosta <47223074+otavioalbucosta@users.noreply.github.com>
2019-08-29 17:29:21 -0300

    Add files via upload

--- GeniusLyrics/Genius.py
+++ GeniusLyrics/Genius.py
@@ -1,10 +1,13 @@
-import requests
 import bs4
+import requests
+import pytube
 base_url = "https://api.genius.com/"
+youtube_url = "https://www.googleapis.com/youtube/v3/search"
 search_url = base_url + "search/"
 cli_id="NQP3U7EBpW_h2ivnQlecMD92f5dUFfmiNV8drV81dQaodxiyeWjzdRVfLKVshpb2"
 cli_tok="jgS62kc6s_PBjP3e0P7q7qR4lzC125MyqWWxhrLjn11o1Ndfh9cZ58wEe5IVnnRi"
-header = {'Authorization':'Bearer '+ cli_tok}
+yt_key = "AIzaSyBnrEKxA6QZ6hTuNjj4LRuk9MDdQtI8NNA"
+geniusHeader = {'Authorization':'Bearer '+ cli_tok}
 def getLyric(author_name,music_title):
     if music_title == '':
         data = {'q':author_name}
@@ -12,7 +15,7 @@ def getLyric(author_name,music_title):
         data = {'q': music_title}
     else:
         data = {'q':author_name + " " + music_title}
-    req = requests.get(search_url, data=data,headers=header)
+    req = requests.get(search_url, data=data,headers=geniusHeader)
     req = req.json()
     i = int(1)
     print(req['response']['hits'])
@@ -26,8 +29,18 @@ def getLyric(author_name,music_title):
     lyrics = text.find('div', class_='lyrics').get_text()
     return lyrics
 
-a=input("Author name")
-b= input("Music name")
-req = getLyric(a,b)
+def downloadMusic(query,musicLink = ""):
 
-print(req)
+    data = {'q':query,'part':'snippet','key':yt_key}
+    req = requests.get(youtube_url,params=data)
+    req = req.json()
+    print(req)
+
+
+
+#a=input("Author name")
+#b= input("Music name")
+#req = getLyric(a,b)
+
+a = input("namaewa")
+downloadMusic(a)

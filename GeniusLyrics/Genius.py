6992bbc47e165c8649ac68de49631987d4f5dc93
otavioalbucosta <47223074+otavioalbucosta@users.noreply.github.com>
2019-08-30 13:35:14 -0300

    Add files via upload

--- GeniusLyrics/Genius.py
+++ GeniusLyrics/Genius.py
@@ -1,38 +1,42 @@
 import bs4
 import requests
 import pytube
-import moviepy.editor
-base_url = "https://api.genius.com/"
+import subprocess
+import os
+import pygame
+
+## Tokens and base links ##
+genius_url = "https://api.genius.com/"
 youtube_url = "https://www.googleapis.com/youtube/v3/search"
-search_url = base_url + "search/"
-cli_id="NQP3U7EBpW_h2ivnQlecMD92f5dUFfmiNV8drV81dQaodxiyeWjzdRVfLKVshpb2"
+search_url = genius_url + "search/"
 cli_tok="jgS62kc6s_PBjP3e0P7q7qR4lzC125MyqWWxhrLjn11o1Ndfh9cZ58wEe5IVnnRi"
 yt_key = "AIzaSyBnrEKxA6QZ6hTuNjj4LRuk9MDdQtI8NNA"
 geniusHeader = {'Authorization':'Bearer '+ cli_tok}
-def getLyric(author_name,music_title):
-    if music_title == '':
-        data = {'q':author_name}
-    elif author_name == '':
-        data = {'q': music_title}
-    else:
-        data = {'q':author_name + " " + music_title}
-    req = requests.get(search_url, data=data,headers=geniusHeader)
+
+## Pygame Start ##
+pygame.mixer.init()
+
+
+def getLyric(search):
+    data = {'q':search}
+    req = requests.get(search_url, data=data, headers=geniusHeader)
     req = req.json()
     i = int(1)
     print(req['response']['hits'])
     for hit in req['response']['hits']:
-        print(str(i) + ': '+  hit['result']['title'] + " - " + hit['result']["primary_artist"]["name"])
-        i+=1
+        print(str(i) + ': ' + hit['result']['title'] + " - " + hit['result']["primary_artist"]["name"])
+        i += 1
     val = int(input("Digite a musica que deseja de acordo com o numero"))
     link = req['response']['hits'][val-1]['result']['url']
     pagereq = requests.get(link)
-    text  = bs4.BeautifulSoup(pagereq.text, 'html.parser')
+    text = bs4.BeautifulSoup(pagereq.text, 'html.parser')
     lyrics = text.find('div', class_='lyrics').get_text()
     return lyrics
 
+
 def searchMusic(query):
 
-    data = {'q':query,'part':'snippet','key':yt_key}
+    data = {'q': query, 'part': 'snippet', 'key': yt_key}
     req = requests.get(youtube_url,params=data)
     print(req.url)
     req = req.json()
@@ -43,7 +47,9 @@ def searchMusic(query):
     val = int(input("digite o numero do vídeo desejado\n"))
     return "http://youtube.com/watch?v=" + req['items'][val-1]['id']['videoId']
 
+
 def downloadMusic(link):
+
     titlelnk = link.replace('http://youtube.com/watch?v=','')
     data = {'q': titlelnk, 'part': 'snippet', 'key': yt_key}
     titreq = requests.get(youtube_url,params=data)
@@ -56,16 +62,23 @@ def downloadMusic(link):
         print(video.itag)
         if video.mime_type == "audio/mp4":
             video.download(filename=titreq)
+            convert_video(titreq+'.mp4',titreq+".mp3")
+            os.remove(titreq + '.mp4')
             break
+    return titreq
 
 
+def convert_video(video_input, video_output):
 
+    cmds = ['ffmpeg', '-i', video_input, video_output]
+    subprocess.Popen(cmds).wait()
 
-
-
-#a=input("Author name")
-#b= input("Music name")
-#req = getLyric(a,b)
 
 a = input("namaewa")
-downloadMusic(searchMusic(a))
+videoName = downloadMusic(searchMusic(a))
+
+a=input("Nome da música com autor, de preferencia")
+req = getLyric(a)
+print(req)
+pygame.mixer.music.load(videoName+".mp3")
+pygame.mixer.music.play()
